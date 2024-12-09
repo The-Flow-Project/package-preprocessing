930e65d4b80ef5036e5b1432e9f7bdc9a7cbd43b
danameyer <dmeyer6@uni-bielefeld.de>
2024-12-09 11:21:51 +0100

    adapt test for preprocess function to fit new initialisation scheme. Refs #5.

--- tests/unit_tests/preprocess_test.py
+++ tests/unit_tests/preprocess_test.py
@@ -17,15 +17,17 @@ class PreprocessTest(unittest.TestCase):
             os.makedirs(self.in_path_github)
         self.xml_files: List[str] = [os.path.join(self.in_path, filename) for filename in os.listdir(self.in_path) if
                                      filename.endswith(".xml")]
-        self.preprocessor = Preprocessor()
-        self.process_id = 'test_process'
-        self.repo_name = "github-actions-test-organisation/github-interaction-test"
-        self.repo_folder = "xml"
+        process_id = 'test_process'
+        repo_name = "github-actions-test-organisation/github-interaction-test"
+        repo_folder = "xml"
+        github_access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
+        self.preprocessor = Preprocessor(github_access_token=github_access_token,
+                                         process_id=process_id,
+                                         repo_name=repo_name,
+                                         repo_folder=repo_folder)
 
     def test_preprocess_with_github(self):
-        asyncio.run(self.preprocessor.preprocess(process_id=self.process_id,
-                                                 repo_name=self.repo_name,
-                                                 repo_folder=self.repo_folder))
+        asyncio.run(self.preprocessor.preprocess())
 
     def test_preprocess_crop_false(self):
         asyncio.run(self.preprocessor.preprocess_xml_file_list(self.xml_files, self.in_path, self.out_path))
@@ -34,7 +36,8 @@ class PreprocessTest(unittest.TestCase):
         asyncio.run(self.preprocessor.preprocess_xml_file_list(self.xml_files, self.in_path, self.out_path, crop=True))
 
     def test_preprocess_abbrev_true(self):
-        asyncio.run(self.preprocessor.preprocess_xml_file_list(self.xml_files, self.in_path, self.out_path, abbrev=True))
+        asyncio.run(
+            self.preprocessor.preprocess_xml_file_list(self.xml_files, self.in_path, self.out_path, abbrev=True))
 
     # def tearDown(self) -> None:
     #     """
