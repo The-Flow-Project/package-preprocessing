fb3e7c01df21b6fc3cfee6e60ec7fcd939422751
Jonas Widmer <jonas.widmer@unibe.ch>
2025-10-09 16:47:15 +0200

    replace logging file with inline dict

--- src/flow_preprocessor/utils/logging/preprocessing_logger.py
+++ src/flow_preprocessor/utils/logging/preprocessing_logger.py
@@ -4,19 +4,55 @@ Create preprocessing logger
 
 import os
 import logging.config
-import yaml
 
 # Ensure log directory exists
 LOG_DIR = "logs"
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'detailed': {
+            'format': '%(asctime)s - %(name)s - %(filename)s - %(levelname)s - %(message)s',
+        },
+        'brief': {
+            'format': '%(levelname)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'detailed',
+            'filename': 'logs/preprocessing.log',
+            'mode': 'a',
+            'encoding': 'utf-8',
+            'maxBytes': 10485760,
+            'backupCount': 5,
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'brief',
+        },
+    },
+    'loggers': {
+        'preprocessing_logger': {
+            'level': 'INFO',
+            'handlers': ['file', 'console'],
+            'propagate': False,
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['file', 'console'],
+    },
+}
+
 # Load logging configuration
-current_dir = os.path.dirname(os.path.realpath(__file__))
-logging_config = os.path.join(current_dir, "logging_config.yaml")
-with open(logging_config, encoding='utf-8') as file:
-    config = yaml.safe_load(file)
-    logging.config.dictConfig(config)
+logging.config.dictConfig(logging_config)
 
 # Central logger accessible throughout the application
 logger = logging.getLogger("preprocessing_logger")
